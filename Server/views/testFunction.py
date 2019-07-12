6e70a2ce3c66dd30a08f38ba92d8570db1af2641
Florea Marius <47918060+mariusflorea98@users.noreply.github.com>
2019-07-12 15:42:46 +0300

    Add files via upload

--- Server/views/testFunction.py
+++ Server/views/testFunction.py
@@ -11,15 +11,19 @@ import matplotlib.pyplot as plt
 import np
 @test_function.route('/test',methods=["POST", "GET"])
 def test():
-    json_data_from_request = request.get_json()
+    json_data_from_request = request.get_json()         #{'data': data}
 
     img=json_data_from_request['data']
-
-    base64result = img.split(',')[1]
+    #print(img)
+    base64result = img.split(',')[1]                    #remove data:image/png;base64 header
     #print(base64result)
 
-    imgdata = base64.b64decode(str(base64result))
+    imgdata = base64.b64decode(str(base64result))       #Decode the Base64 encoded bytes-like object or ASCII string and return the decoded bytes.
+
     image = Image.open(io.BytesIO(imgdata))
-    plt.imshow(image)
-    plt.show()
-    return imgdata
+    #plt.imshow(image)
+    #plt.show()                                          #plot image
+
+    imag={'data':img}                                    #attempt to send image from flask to web
+
+    return imag
